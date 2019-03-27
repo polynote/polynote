e4e5b8f2c97f4cdd2387ab6853b04f9aadc75291
Jonathan Indig <jindig@netflix.com>
2019-03-27 16:19:34 -0700

    fix environment variables and properly launch polynote so it can actually read them

--- scripts/install-jep.sh
+++ scripts/install-jep.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
 pip3 install jep ipython nbconvert jedi
-export LD_LIBRARY_PATH=`pip3 show jep |grep "^Location:" |cut -d ':' -f 2 |cut -d ' ' -f 2`/jep:${LD_LIBRARY_PATH}
+
+# this all assumes that jep was installed to a normal place
+jep_site_packages_path=`pip3 show jep |grep "^Location:" |cut -d ':' -f 2 |cut -d ' ' -f 2`
+jep_path=${jep_site_packages_path}/jep
+jep_lib_path=`realpath ${jep_site_packages_path}/../../`
+
+export LD_LIBRARY_PATH=${jep_path}:${jep_site_packages_path}:${jep_lib_path}:${LD_LIBRARY_PATH}
 echo ${LD_LIBRARY_PATH}
+
+export LD_PRELOAD=${jep_lib_path}/libpython3.6m.so
+echo ${LD_PRELOAD}
