a9bede225a05a006123a29db21676acc77488483
Jonathan Indig <jindig@netflix.com>
2019-08-01 12:51:56 -0700

    reload page upon reconnecting

--- polynote-frontend/polynote/comms.js
+++ polynote-frontend/polynote/comms.js
@@ -148,7 +148,7 @@ export class SocketSession extends EventTarget {
     reconnect(onlyIfClosed) {
         if (!this.socket || this.isClosed || (!onlyIfClosed && (this.socket.readyState > WebSocket.CONNECTING))) {
             this.close();
-            mkSocket.call(this);
+            document.location.reload();
         }
     }
 
