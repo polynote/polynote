7a6f921a18d68300ea0ebbfc59f074b7fdff93b0
Omid Mogasemi <omidmogasemi@gmail.com>
2022-08-30 16:00:19 -0700

    Add dropdowns for each sub-section in config & dependencies section

--- polynote-frontend/polynote/ui/component/notebook/notebookconfig.ts
+++ polynote-frontend/polynote/ui/component/notebook/notebookconfig.ts
@@ -34,12 +34,12 @@ export class NotebookConfigEl extends Disposable {
         super()
 
         const configState = stateHandler.view("config");
-        const dependencies = new Dependencies(configState.view("dependencies"));
-        const exclusions = new Exclusions(configState.view("exclusions"));
-        const resolvers = new Resolvers(configState.view("repositories"));
+        const dependencies = new Dependencies(configState.view("dependencies"), stateHandler);
+        const exclusions = new Exclusions(configState.view("exclusions"), stateHandler);
+        const resolvers = new Resolvers(configState.view("repositories"), stateHandler);
         const serverTemplatesHandler = ServerStateHandler.view("sparkTemplates").disposeWith(configState);
-        const spark = new SparkConf(configState.view("sparkConfig"), configState.view("sparkTemplate"), serverTemplatesHandler);
-        const kernel = new KernelConf(configState);
+        const spark = new SparkConf(configState.view("sparkConfig"), configState.view("sparkTemplate"), serverTemplatesHandler, stateHandler);
+        const kernel = new KernelConf(configState, stateHandler);
 
         const saveButton = button(['save'], {}, ['Save & Restart']).click(evt => {
             const conf = new NotebookConfig(dependencies.conf, exclusions.conf, resolvers.conf, spark.conf, spark.template, kernel.envVars, kernel.scalaVersion, kernel.jvmArgs);
@@ -93,14 +93,16 @@ class Dependencies extends Disposable {
     readonly el: TagElement<"div">;
     private container: TagElement<"div">;
 
-    constructor(dependenciesHandler: StateView<Record<string, string[]> | undefined>) {
+    constructor(dependenciesHandler: StateView<Record<string, string[]> | undefined>, stateHandler: StateHandler<NBConfig>) {
         super()
 
-        this.el = div(['notebook-dependencies', 'notebook-config-section'], [
-            h3([], ['Dependencies']),
-            para([], ['You can provide Scala / JVM dependencies using  Maven coordinates , e.g. ', span(['pre'], ['org.myorg:package-name_2.11:1.0.1']), ', or URLs like ', span(['pre'], ['s3://path/to/my.jar'])]),
-            para([], ['You can also specify pip packages, e.g. ', span(['pre'], ['requests']), ', or with a version like ', span(['pre'], ['urllib3==1.25.3'])]),
-            this.container = div(['dependency-list'], [])
+        this.el = div(['notebook-dependencies', 'notebook-config-section', 'open'], [
+            h3([], ['Dependencies']).click(() => stateHandler.updateField('openDependencies', openDependencies => setValue(!openDependencies))),
+            div(['notebook-config-section-content'], [
+                para([], ['You can provide Scala / JVM dependencies using  Maven coordinates , e.g. ', span(['pre'], ['org.myorg:package-name_2.11:1.0.1']), ', or URLs like ', span(['pre'], ['s3://path/to/my.jar'])]),
+                para([], ['You can also specify pip packages, e.g. ', span(['pre'], ['requests']), ', or with a version like ', span(['pre'], ['urllib3==1.25.3'])]),
+                this.container = div(['dependency-list'], [])
+            ])
         ])
 
         const setDeps = (deps: Record<string, string[]> | undefined) => {
@@ -122,6 +124,14 @@ class Dependencies extends Disposable {
         }
         setDeps(dependenciesHandler.state)
         dependenciesHandler.addObserver(deps => setDeps(deps)).disposeWith(this)
+
+        stateHandler.view('openDependencies').addObserver(open => {
+            if (open) {
+                this.el.classList.add("open")
+            } else {
+                this.el.classList.remove("open")
+            }
+        }).disposeWith(this)
     }
 
     private defaultLang = "scala"; // TODO: make this configurable
@@ -194,13 +204,15 @@ class Resolvers extends Disposable {
     readonly el: TagElement<"div">;
     private container: TagElement<"div">;
 
-    constructor(resolversHandler: StateView<RepositoryConfig[] | undefined>) {
+    constructor(resolversHandler: StateView<RepositoryConfig[] | undefined>, stateHandler: StateHandler<NBConfig>) {
         super()
 
-        this.el = div(['notebook-resolvers', 'notebook-config-section'], [
-            h3([], ['Resolvers']),
-            para([], ['Specify any custom Ivy, Maven, or Pip repositories here.']),
-            this.container = div(['resolver-list'], [])
+        this.el = div(['notebook-resolvers', 'notebook-config-section', 'open'], [
+            h3([], ['Resolvers']).click(() => stateHandler.updateField('openResolvers', openDependencies => setValue(!openDependencies))),,
+            div(['notebook-config-section-content'], [
+                para([], ['Specify any custom Ivy, Maven, or Pip repositories here.']),
+                this.container = div(['resolver-list'], [])
+            ])
         ])
 
         const setResolvers = (resolvers: RepositoryConfig[] | undefined) => {
@@ -232,6 +244,10 @@ class Resolvers extends Disposable {
 
         setResolvers(resolversHandler.state)
         resolversHandler.addObserver(resolvers => setResolvers(resolvers)).disposeWith(this)
+
+        stateHandler.view('openResolvers').addObserver(open => {
+            toggleConfigVisibility(open, this.el);
+        }).disposeWith(this)
     }
 
     private defaultRes = "ivy"; // TODO: make this configurable
@@ -301,13 +317,15 @@ class Exclusions extends Disposable {
     readonly el: TagElement<"div">;
     private container: TagElement<"div">;
 
-    constructor(exclusionsHandler: StateView<string[] | undefined>) {
+    constructor(exclusionsHandler: StateView<string[] | undefined>, stateHandler: StateHandler<NBConfig>) {
         super()
 
-        this.el = div(['notebook-exclusions', 'notebook-config-section'], [
-            h3([], ['Exclusions']),
-            para([], ['[Scala only]: Specify organization:module coordinates for your exclusions, i.e. ', span(['pre'], ['org.myorg:package-name_2.11'])]),
-            this.container = div(['exclusion-list'], [])
+        this.el = div(['notebook-exclusions', 'notebook-config-section', 'open'], [
+            h3([], ['Exclusions']).click(() => stateHandler.updateField('openExclusions', openDependencies => setValue(!openDependencies))),,
+            div(['notebook-config-section-content'], [
+                para([], ['[Scala only]: Specify organization:module coordinates for your exclusions, i.e. ', span(['pre'], ['org.myorg:package-name_2.11'])]),
+                this.container = div(['exclusion-list'], [])
+            ])
         ])
 
         const setExclusions = (exclusions: string[] | undefined) => {
@@ -323,6 +341,10 @@ class Exclusions extends Disposable {
         }
         setExclusions(exclusionsHandler.state)
         exclusionsHandler.addObserver(excl => setExclusions(excl)).disposeWith(this)
+
+        stateHandler.view('openExclusions').addObserver(open => {
+            toggleConfigVisibility(open, this.el);
+        }).disposeWith(this)
     }
 
     private addExcl(item?: string) {
@@ -359,16 +381,19 @@ class SparkConf extends Disposable {
     private container: TagElement<"div">;
     private templateEl: DropdownElement;
 
-    constructor(confHandler: StateView<Record<string, string> | undefined>, templateHandler: StateView<SparkPropertySet | undefined>, private allTemplatesHandler: StateView<SparkPropertySet[]>) {
+    constructor(confHandler: StateView<Record<string, string> | undefined>, templateHandler: StateView<SparkPropertySet | undefined>,
+                private allTemplatesHandler: StateView<SparkPropertySet[]>, stateHandler: StateHandler<NBConfig>) {
         super()
 
         this.templateEl = dropdown([], Object.fromEntries([["", "None"]]), );
         this.container = div(['spark-config-list'], []);
 
-        this.el = div(['notebook-spark-config', 'notebook-config-section'], [
-            h3([], ['Spark configuration']),
-            para([], ['Set Spark configuration for this notebook here. Please note that it is possible that your environment may override some of these settings at runtime :(']),
-            div([], [h4([], ['Spark template:']), this.templateEl, h4([], ['Spark properties:']), this.container])
+        this.el = div(['notebook-spark-config', 'notebook-config-section', 'open'], [
+            h3([], ['Spark configuration']).click(() => stateHandler.updateField('openSpark', openDependencies => setValue(!openDependencies))),,
+            div(['notebook-config-section-content'], [
+                para([], ['Set Spark configuration for this notebook here. Please note that it is possible that your environment may override some of these settings at runtime :(']),
+                div([], [h4([], ['Spark template:']), this.templateEl, h4([], ['Spark properties:']), this.container])
+            ])
         ])
 
         const setConf = (conf: Record<string, string> | undefined) => {
@@ -400,6 +425,10 @@ class SparkConf extends Disposable {
         }
         setTemplate(templateHandler.state)
         templateHandler.addObserver(template => setTemplate(template)).disposeWith(this)
+
+        stateHandler.view('openSpark').addObserver(open => {
+            toggleConfigVisibility(open, this.el);
+        }).disposeWith(this)
     }
 
     private addConf(item?: {key: string, val: string}) {
@@ -448,7 +477,7 @@ class KernelConf extends Disposable {
     private jvmArgsInput: TagElement<"input">;
     private scalaVersionInput: DropdownElement;
 
-    constructor(configState: StateView<NotebookConfig>) {
+    constructor(configState: StateView<NotebookConfig>, stateHandler: StateHandler<NBConfig>) {
         super();
         const envHandler = configState.view("env");
         const jvmArgsHandler = configState.view("jvmArgs");
@@ -461,17 +490,19 @@ class KernelConf extends Disposable {
             "2.13": "2.13"
         }
 
-        this.el = div(['notebook-env', 'notebook-config-section'], [
-            h3([], ['Kernel configuration']),
-            para([], ['Please note this is only supported when kernels are launched as a subprocess (default).']),
-            h4([], 'Scala version:'),
-            para([], `If using Spark, the Scala version must match that of your Spark installation. "Default" will use Polynote's configured Scala version, or auto-detect the appropriate version.`),
-            this.scalaVersionInput = dropdown(['scala-version'], {"": "Default", ...availableScalaVersions}, scalaVersionHandler.state || ""),
-            h4([], 'Environment variables:'),
-            this.container = div(['env-list'], []),
-            h4([], ['Additional JVM arguments:']),
-            para([], ['Extra command-line arguments to the JVM, e.g. ', tag('code', [], {}, '"-Dmy.prop=a value" -Xmx200m')]),
-            this.jvmArgsInput = textbox(['jvm-args'], "JVM Arguments", joinQuotedArgs(jvmArgsHandler.state)).attr("size", "64")
+        this.el = div(['notebook-env', 'notebook-config-section', 'open'], [
+            h3([], ['Kernel configuration']).click(() => stateHandler.updateField('openKernel', openDependencies => setValue(!openDependencies))),,
+            div(['notebook-config-section-content'], [
+                para([], ['Please note this is only supported when kernels are launched as a subprocess (default).']),
+                h4([], 'Scala version:'),
+                para([], `If using Spark, the Scala version must match that of your Spark installation. "Default" will use Polynote's configured Scala version, or auto-detect the appropriate version.`),
+                this.scalaVersionInput = dropdown(['scala-version'], {"": "Default", ...availableScalaVersions}, scalaVersionHandler.state || ""),
+                h4([], 'Environment variables:'),
+                this.container = div(['env-list'], []),
+                h4([], ['Additional JVM arguments:']),
+                para([], ['Extra command-line arguments to the JVM, e.g. ', tag('code', [], {}, '"-Dmy.prop=a value" -Xmx200m')]),
+                this.jvmArgsInput = textbox(['jvm-args'], "JVM Arguments", joinQuotedArgs(jvmArgsHandler.state)).attr("size", "64")
+            ])
         ])
 
         const setEnv = (env: Record<string, string> | undefined) => {
@@ -488,6 +519,10 @@ class KernelConf extends Disposable {
         setEnv(envHandler.state);
         envHandler.addObserver(env => setEnv(env));
         jvmArgsHandler.addObserver(strs => this.jvmArgsInput.value = joinQuotedArgs(strs) || "");
+
+        stateHandler.view('openKernel').addObserver(open => {
+            toggleConfigVisibility(open, this.el);
+        }).disposeWith(this)
     }
 
     private addEnv(item?: {key: string, val: string}) {
@@ -532,5 +567,13 @@ class KernelConf extends Disposable {
 
     get scalaVersion(): string | undefined {
         return this.scalaVersionInput.getSelectedValue() || undefined;
+    }
+}
+
+function toggleConfigVisibility(open: boolean, el: HTMLDivElement) {
+    if (open) {
+        el.classList.add("open")
+    } else {
+        el.classList.remove("open")
     }
 }
