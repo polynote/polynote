e68c4c924d84d260e0f6c9ae12e8e3ffcfdbf7c3
Omid Mogasemi <omidmogasemi@gmail.com>
2022-08-31 11:19:05 -0700

    Try to fix jest canvas issue

--- polynote-frontend/jest.config.js
+++ polynote-frontend/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'ts-jest/presets/js-with-ts',
   testMatch: ["**/?(*.)+(spec|test).ts"],
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-  setupFilesAfterEnv: ['./jest.setup.ts', 'fake-indexeddb/auto'],
+  setupFilesAfterEnv: ['./jest.setup.ts', 'fake-indexeddb/auto', 'jest-canvas-mock'],
   transform: {
     "^.+\\.ts$": "ts-jest"
   },
