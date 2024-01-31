9dab6430e8c113249702aa0bda73777318ec9377
jonathanindig <5430417+jonathanindig@users.noreply.github.com>
2024-01-30 20:35:41 -0500

    Defensively add more comprehensive set of paths to PYTHONPATH and LD_LIBRARY_PATH

--- scripts/polynote.py
+++ scripts/polynote.py
@@ -4,8 +4,15 @@ from pathlib import Path
 import os
 import shlex
 
+# Depending on how Python is installed, sometimes it's hard to find jep or other important libraries (e.g., libpython). 
+# The sys module contains a bunch of *_prefix attributes that point to various locations where these libraries might be, 
+# such as https://docs.python.org/3/library/sys.html#sys.exec_prefix 
+# We set both the PYTHONPATH and the LD_LIBRARY_PATH just in case
+sys_prefixes = {getattr(sys, sys_prefix) for sys_prefix in filter(lambda name: "prefix" in name, dir(sys))}
 if not os.environ.get('PYTHONPATH'):
-    os.environ['PYTHONPATH'] = sys.prefix
+    os.environ['PYTHONPATH'] = os.pathsep.join(sys_prefixes)
+if not os.environ.get('LD_LIBRARY_PATH'):
+    os.environ['LD_LIBRARY_PATH'] = os.pathsep.join([os.path.join(path, "lib") for path in sys_prefixes])
 
 scala_version = os.environ.get('POLYNOTE_SCALA_VERSION', '2.11')
 
