c4088cd657746b2004a9dc85c83671f812e7b98c
steve layland <stevieplayland@gmail.com>
2024-04-16 18:06:17 +0800

    github.dev test

--- scripts/polynote.py
+++ scripts/polynote.py
@@ -7,8 +7,8 @@ import shlex
 if not os.environ.get('PYTHONPATH'):
     os.environ['PYTHONPATH'] = sys.prefix
 
-polynote_dir = os.path.dirname(os.path.realpath(__file__))
-os.chdir(polynote_dir)
+polynote_dir = Path(__file__).resolve().parent
+polynote_dir.chdir()
 
 paths = [ Path(p) for p in sys.path if Path(p).exists() ]
 jep_paths = [ p.joinpath("jep") for p in paths if p.joinpath("jep").exists() ]
@@ -18,18 +18,18 @@ if len(jep_paths) >= 1:
 else:
     raise Exception("Couldn't find jep library. Try running `pip3 install jep` first.")
 
-plugins_path = Path(polynote_dir).joinpath("plugins.d")
+plugins_path = polynote_dir / "plugins.d"
 plugins = []
 
 if plugins_path.exists():
     plugins = list(plugins_path.glob("*.jar"))
 
-deps_path = Path(polynote_dir).joinpath("deps")
+deps_path = polynote_dir / "deps"
 
 if not(deps_path.exists()):
     raise Exception("Couldn't find the deps directory. Are we in the polynote installation directory?")
 
-deps = Path(polynote_dir).joinpath("deps").glob("*.jar")
+deps = deps_path.glob("*.jar")
 classpath = "polynote.jar:" + ":".join([":".join([ f'"{d}"' for d in deps ]), ":".join([ f'"{p}"' for p in plugins ])])
 cmd = f"java -cp polynote.jar:{classpath} -Djava.library.path={jep_path} polynote.Main {' '.join(sys.argv[1:])}"
 cmd = shlex.split(cmd)
